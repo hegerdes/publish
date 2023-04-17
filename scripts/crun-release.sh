a5856bdbd105f72674c30ef8f1dc272e4f63fc4e
Henrik Gerdes <hegerdes@outlook.de>
2023-04-17 19:23:50 +0200

    test multi-arch

--- scripts/crun-release.sh
+++ scripts/crun-release.sh
@@ -36,3 +36,53 @@ make dist-xz
 mv crun-*.tar.gz $OUTDIR
 mv crun-*.tar.xz $OUTDIR
 
+make distclean
+
+RUNTIME=${RUNTIME:-podman}
+RUNTIME_EXTRA_ARGS=${RUNTIME_EXTRA_ARGS:-}
+
+mkdir -p /nix
+
+NIX_ARGS="--extra-experimental-features nix-command --print-build-logs --option cores $(nproc) --option max-jobs $(nproc)"
+
+$RUNTIME run --rm $RUNTIME_EXTRA_ARGS --privileged -v /nix:/nix -v ${PWD}:${PWD} -w ${PWD} ${NIX_IMAGE} \
+    nix $NIX_ARGS build --file nix/
+cp ./result/bin/crun $OUTDIR/crun-$VERSION-linux-amd64
+
+rm -rf result
+
+$RUNTIME run --rm $RUNTIME_EXTRA_ARGS --privileged -v /nix:/nix -v ${PWD}:${PWD} -w ${PWD} ${NIX_IMAGE} \
+    nix $NIX_ARGS build --file nix/ --arg enableSystemd false
+cp ./result/bin/crun $OUTDIR/crun-$VERSION-linux-amd64-disable-systemd
+
+rm -rf result
+
+$RUNTIME run --rm $RUNTIME_EXTRA_ARGS --privileged -v /nix:/nix -v ${PWD}:${PWD} -w ${PWD} ${NIX_IMAGE} \
+    nix $NIX_ARGS build --file nix/default-arm64.nix
+cp ./result/bin/crun $OUTDIR/crun-$VERSION-linux-arm64
+
+rm -rf result
+
+$RUNTIME run --rm $RUNTIME_EXTRA_ARGS --privileged -v /nix:/nix -v ${PWD}:${PWD} -w ${PWD} ${NIX_IMAGE} \
+    nix $NIX_ARGS build --file nix/default-arm64.nix --arg enableSystemd false
+cp ./result/bin/crun $OUTDIR/crun-$VERSION-linux-arm64-disable-systemd
+
+rm -rf result
+
+$RUNTIME run --rm $RUNTIME_EXTRA_ARGS --privileged -v /nix:/nix -v ${PWD}:${PWD} -w ${PWD} ${NIX_IMAGE} \
+    nix $NIX_ARGS build --file nix/default-ppc64le.nix
+cp ./result/bin/crun $OUTDIR/crun-$VERSION-linux-ppc64le
+
+rm -rf result
+
+$RUNTIME run --rm $RUNTIME_EXTRA_ARGS --privileged -v /nix:/nix -v ${PWD}:${PWD} -w ${PWD} ${NIX_IMAGE} \
+    nix $NIX_ARGS build --file nix/default-ppc64le.nix --arg enableSystemd false
+cp ./result/bin/crun $OUTDIR/crun-$VERSION-linux-ppc64le-disable-systemd
+
+rm -rf result
+
+if test x$SKIP_GPG = x; then
+    for i in $OUTDIR/*; do
+        gpg2 -b --armour $i
+    done
+fi
