31c54d700607096b59a49c1a587e844e88e575db
Henrik Gerdes <hegerdes@outlook.de>
2024-05-21 21:36:51 +0200

    feat: add aws role for gh action deploy via tf
    Signed-off-by: Henrik Gerdes <hegerdes@outlook.de>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# publish
-Contains different assets and binaries
+# Publish
+Contains different assets and binaries for shared projects.
+
+## Content
+ * Reusable github workflows
+ * http-get-push - A lambda function to make http GET's to POST's
+ * crun - Containerd runc alternative, compiled with WASM support
--- http-get-push/terraform/main.tf
+++ http-get-push/terraform/main.tf
@@ -21,7 +21,7 @@ locals {
 
   tags = {
     owner       = "Henrik Gerdes"
-    cost-center = "unknown"
+    cost-center = "default"
     project     = "http-get-push"
     status      = "active"
     managed-by  = "terraform"
@@ -54,9 +54,75 @@ module "lambda" {
   runtime                           = "nodejs20.x"
   architectures                     = ["arm64"]
   authorization_type                = "NONE"
+  role_name                         = "lambda-${local.binary_name}"
   create_lambda_function_url        = true
   create_sam_metadata               = true
   cloudwatch_logs_retention_in_days = 14
   function_tags                     = local.tags
   cloudwatch_logs_tags              = local.tags
+}
+
+
+# ################################################################################
+# # IAM external connections
+# ################################################################################
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-hegerdes-publish-http-get-push"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+  tags               = local.tags
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name        = "github-actions-http-get-push"
+  description = "Grant Github Actions the ability to push the labda code from hegerdes/publish"
+  policy      = data.aws_iam_policy_document.github_actions.json
+  tags        = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
+
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:hegerdes/publish:*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "iam:ListRoles",
+      "lambda:UpdateFunctionCode",
+      "lambda:UpdateFunctionConfiguration",
+      "lambda:GetFunctionConfiguration",
+      "lambda:InvokeFunction",
+      "lambda:CreateFunction",
+      "lambda:GetFunction",
+    ]
+    resources = [module.lambda.lambda_function_arn]
+  }
+
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
 }
